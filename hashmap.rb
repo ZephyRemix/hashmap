d63aa6b6c05ba0b01f38ba06fab34e0b1185de8e
ZephyRemix <hunyaochong@gmail.com>
2024-08-09 09:08:27 +0800

    Add hash function

--- hashmap.rb
+++ hashmap.rb
@@ -1,9 +1,32 @@
 class HashMap
   def initialize
-    
-  end 
+  end
 
   def hash(key)
+    hash_code = 0
+    prime_number = 31
+       
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+       
+    hash_code
+  end
+
+  def set(key, value)
     
-  end 
+  end
 end 
+
+test = HashMap.new
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
